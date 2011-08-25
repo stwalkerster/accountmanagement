7ad64c98011ac695bbfdfc3a4a3ab36b0e17ace5
Simon Walker <github@helpmebot.org.uk>
2011-08-25 21:11:34 +0100

    login form

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php include("head.html"); ?>
-    <h2>Account management</h2>
+    <h2>Account management - Login</h2>
+	<form>
+	
+	<input type="text" name="uid" />
+	<input type="password" name="pw" />
+	<input type="checkbox" name="rdn" value="y" />
+	<input type="submit" value="Login" />
+	</form>
 <?php include("foot.html"); ?>    
 
