be1e485119415abaa6f59e58537cbc4202a4a8c9
Simon Walker <github@helpmebot.org.uk>
2011-08-25 19:49:37 +0000

    initial import

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Operations Centre</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" type="text/css"  href="http://operations.helpmebot.org.uk/style.css" />
+</head>
+<body>
+<div id="wrapper">
+  <div id="nav">
+    <h1>Operations Centre Account Management</h1>
+    <ul id="menu">
+      <li><a href="http://operations.helpmebot.org.uk/">Home</a></li>
+      <li><a href="http://operations.helpmebot.org.uk/account/">Accounts Home</a></li>
+
+    </ul>
+    <img src="/images/main.jpg" alt="" /> </div>
+  <div id="content">
+    <h2>Account management</h2>
+    
+    <div class="line"></div>
+    <div class="footer"></div>
+    <p>helpmebot.org.uk &copy; 2011 - design by <a href="http://templatetower.com">templatetower.com </a> - image adapted from <a href="http://commons.wikimedia.org/wiki/File:Wmf_sdtpa_servers_2009-01-20_34.jpg">original by RobH</a></p>
+    <p>&nbsp;</p>
+  </div>
+</div>
+</body>
+</html>
---
+++ passwd.php
@@ -0,0 +1,50 @@
+<?php
+// basic sequence with LDAP is connect, bind, search, interpret search
+// result, close connection
+
+echo "<h3>LDAP Password Changer</h3>";
+echo "Connecting to directory service...<br />";
+$ds=ldap_connect("directory.helpmebot.org.uk");  // must be a valid LDAP server!
+
+$username = $_REQUEST['u'];
+$password = $_REQUEST['p'];
+
+$rdnstart = "uid=";
+$rdnend = ",ou=People,";
+$basedn = "dc=helpmebot,dc=org,dc=uk";
+
+$dn = $rdnstart . $username . $rdnend . $basedn;
+//$dn = "uid=stwalkerster,ou=People,dc=helpmebot,dc=org,dc=uk";
+
+if ($ds) {
+    ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3);
+
+    echo "Authenticating as $dn...<br />"; 
+    $r=ldap_bind($ds,$dn,$password);     // this is an "anonymous" bind, typically
+                           // read-only access
+    if(!$r)
+    {
+	echo "Failed!";
+	echo "<!-- ".ldap_error($ds)." -->";
+	die();
+    }
+
+    echo "Searching for (uid=".$username.") ...";
+    // Search surname entry
+    $sr=ldap_search($ds, "dc=helpmebot,dc=org,dc=uk", "uid=" . $username);  
+
+    $info = ldap_get_entries($ds, $sr);
+
+    for ($i=0; $i<$info["count"]; $i++) {
+        echo "dn is: " . $info[$i]["dn"] . "<br />";
+        echo "first cn entry is: " . $info[$i]["cn"][0] . "<br />";
+        echo "first email entry is: " . $info[$i]["mail"][0] . "<br /><hr />";
+    }
+
+    echo "Closing connection";
+    ldap_close($ds);
+
+} else {
+    echo "<h4>Unable to connect to LDAP server</h4>";
+}
+
