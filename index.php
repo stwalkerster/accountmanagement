5f97b96c95692b4b75e0492eb512927d5a4bb08e
Simon Walker <github@stwalkerster.co.uk>
2011-08-25 21:28:39 +0100

    layout and form action

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php include("head.html"); ?>
     <h2>Account management - Login</h2>
-	<form>
-	
-	<input type="text" name="uid" />
-	<input type="password" name="pw" />
-	<input type="checkbox" name="rdn" value="y" />
-	<input type="submit" value="Login" />
+	<form action="main.php" method="post">
+	<table>
+	<tr><td>Username:</td><td><input type="text" name="uid" /></td></tr>
+	<tr><td><input type="checkbox" name="rdn" value="y" /></td><td>User ID is distinguished name</td></tr>
+	<tr><td>Password:</td><td><input type="password" name="pw" /></td></tr>
+	<tr><td /><td><input type="submit" value="Login" /></td></tr>
+	</table>
 	</form>
 <?php include("foot.html"); ?>    
 
