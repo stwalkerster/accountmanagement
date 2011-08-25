b3dffdddf3074df49288d3e42cfcba28b32886c3
Simon Walker <github@stwalkerster.co.uk>
2011-08-25 21:32:13 +0100

    layout

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<form action="main.php" method="post">
 	<table>
 	<tr><td>Username:</td><td><input type="text" name="uid" /></td></tr>
-	<tr><td><input type="checkbox" name="rdn" value="y" /></td><td>User ID is distinguished name</td></tr>
+	<tr><td/><td><input type="checkbox" name="rdn" value="y" />User ID is distinguished name</td></tr>
 	<tr><td>Password:</td><td><input type="password" name="pw" /></td></tr>
 	<tr><td /><td><input type="submit" value="Login" /></td></tr>
 	</table>
